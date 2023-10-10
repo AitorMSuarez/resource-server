b6cefac05ce3fd7b103300fe574812f8422a461e
aitor.martinez <aitor.martinez@gertek.eus>
2023-10-10 17:39:05 +0200

    Añadiendo Cors. Y actualizando permisos para el admin.

--- src/main/java/com/ams/resourceserver/config/ResourceServerConfig.java
+++ src/main/java/com/ams/resourceserver/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.ams.resourceserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
@@ -17,13 +18,15 @@ public class ResourceServerConfig {
 	private String issuerUri;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// CORS
+		http.cors(Customizer.withDefaults());
 		return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).oauth2ResourceServer(
 				oauth2 -> oauth2.jwt(jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri)))).build();
 	}
 
 	@Bean
-	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+	JwtAuthenticationConverter jwtAuthenticationConverter() {
 		JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); // El nombre que le hemos dado
 		jwtGrantedAuthoritiesConverter.setAuthorityPrefix(""); // tiene por defecto ROLE_ y no hace falta ponerle nada.
--- src/main/java/com/ams/resourceserver/controller/ResourceController.java
+++ src/main/java/com/ams/resourceserver/controller/ResourceController.java
@@ -14,7 +14,7 @@ import com.ams.resourceserver.dto.MessageDto;
 public class ResourceController {
 
 	@GetMapping("/user")
-	@PreAuthorize("hasAnyAuthority('ROLE_ADMIN','OIDC_USER')")
+	@PreAuthorize("hasAnyAuthority('ROLE_USER','OIDC_USER')")
 	public ResponseEntity<MessageDto> user(Authentication authentication) {
 		return ResponseEntity.ok(new MessageDto("Hello " + authentication.getName()));
 	}
